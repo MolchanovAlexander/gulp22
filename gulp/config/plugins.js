a8963850ab67356dadd68db628e97b5105e9c9ed
Alexander Molchanov <rybkolhoz@gmail.com>
2022-02-03 18:04:23 +0200

    55minGulp22

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -1,5 +1,11 @@
-import replace from "gulp-replace";
+import replace from "gulp-replace";     // replace 
+import plumber from "gulp-plumber";     // handler errors 
+import notify from "gulp-notify";       // messages
+import browserSync from "browser-sync"; // local server autoupdate
 
 export const plugins = {
-    replace: replace
+    replace: replace,
+    plumber: plumber,
+    notify: notify,
+    browsersync: browserSync
 }
